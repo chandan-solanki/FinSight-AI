a37813d8f7daa9707b0bfcac7dda603d8679e975
chandan solanki <chandansolanki618@gmail.com>
2025-11-03 22:04:50 +0530

    create sperate the middleware(clerk) and arcject(middleware)

---
+++ arcjet-middleware.js
@@ -0,0 +1,18 @@
+import arcjet, { createMiddleware, shield, detectBot } from "@arcjet/next";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+export default createMiddleware(aj);
+
+export const config = {
+  matcher: ["/api/:path*"], // Only protect API endpoints
+};
